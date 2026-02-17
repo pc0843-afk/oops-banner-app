2c75bfe6216db54b24a8bcb6d8f572c96aafa43e
pc0843-afk <pc0843@srmist.edu.in>
2026-02-17 14:52:54 +0530

    Create notepad OOPSBannerAppUC4.java
    notepad README.md
    # OOPS Banner App - UC4
    
    UC4 uses String arrays and enhanced for-loops to print a banner.
    
    Concepts:
    - Arrays
    - Loops
    - Modularity
    - Reusability

---
+++ notepad OOPSBannerAppUC4.java
@@ -0,0 +1,17 @@
+public class OOPSBannerAppUC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            "*****************************",
+            "*      OOPS BANNER APP      *",
+            "*      UC4 - ARRAYS        *",
+            "*      & LOOPS DEMO        *",
+            "*****************************"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
